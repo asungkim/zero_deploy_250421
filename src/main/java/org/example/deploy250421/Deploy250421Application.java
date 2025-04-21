9a26d5f892ed3f3bf8743271ebaf275ac7f5c749
asungkim <asungkim007@naver.com>
2025-04-21 11:20:53 +0900

    init

---
+++ src/main/java/org/example/deploy250421/Deploy250421Application.java
@@ -0,0 +1,13 @@
+package org.example.deploy250421;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Deploy250421Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Deploy250421Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/deploy250421/Deploy250421ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.deploy250421;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Deploy250421ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
